d8a7549357d07e2a3133977db10fd473f0c63156
matheus <matheusnferreira.am@gmail.com>
2025-01-31 01:04:56 -0300

    //

--- app/src/main/java/com/example/exercisecompose/Navigation.kt
+++ app/src/main/java/com/example/exercisecompose/Navigation.kt
@@ -1,2 +1,16 @@
 package com.example.exercisecompose
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation(navController: NavHostController = rememberNavController() ) {
+    NavHost(
+        navController = navController,
+        startDestination = "tutorial"
+    )
+}
--- app/src/main/java/com/example/exercisecompose/TaskScreen.kt
+++ app/src/main/java/com/example/exercisecompose/TaskScreen.kt
@@ -1,2 +1,29 @@
 package com.example.exercisecompose
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun TaskManager(text: String, modifier: Modifier = Modifier) {
+    Column {
+        Image(
+            painter = painterResource(R.drawable.fametro_banner),
+            contentDescription = null
+        )
+        Text("test")
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskManagerPreview() {
+    TaskManager("test")
+}
+
